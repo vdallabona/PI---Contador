de9d9221f1cdee4d0389a5e20940bddabaecf02d
Osvaldo1408exe <osvaldo1408.exe@gmail.com>
2024-07-10 19:09:54 -0300

    alteração de nome de variaveis, abreviaçãao

--- obj/Debug/net8.0-windows/PI - Vini beta login.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/PI - Vini beta login.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PI - Vini beta login")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8274ffa6b93e47d0436dd868e1b25fb5deac307e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27501bf2ee16d98b7dfe1fcdfcd650090bac2292")]
 [assembly: System.Reflection.AssemblyProductAttribute("PI - Vini beta login")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PI - Vini beta login")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
