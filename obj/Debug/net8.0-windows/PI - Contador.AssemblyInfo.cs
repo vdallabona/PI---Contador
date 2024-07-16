c57404ba24ae3f254a3db705de87fabd6c71260a
Victor Ezaki <victor.ezaki@alunos.sc.senac.br>
2024-07-16 19:35:19 -0300

    Alterações finais do CRUD gasto

--- obj/Debug/net8.0-windows/PI - Contador.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/PI - Contador.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PI - Contador")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1a49eae7a62bc069b15bcaede65c2142f8fba2a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c7c8432603429feeb4338b911a403baf10ca66a")]
 [assembly: System.Reflection.AssemblyProductAttribute("PI - Contador")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PI - Contador")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
